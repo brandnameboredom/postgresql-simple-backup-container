b791a304180bfb9b5b8310a7588e021a46d2d8fd
Brad Turner <brad.w.turner@gmail.com>
2018-02-08 09:55:58 -0500

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,1 @@
-#!/bin/sh
-mkdir -p $BACKUP_DATA_DIR
-
-echo "$BACKUP_MINUTE $BACKUP_HOUR * * * /opt/app-root/src/bin/job.sh" > /opt/app-root/src/crontab
-devcron.py /opt/app-root/src/crontab
+echo "0 10 * * * /opt/app-root/src/bin/job.sh" > /etc/cron.d/pg-backup-cron
